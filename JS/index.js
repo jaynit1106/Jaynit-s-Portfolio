5627abee8b40bfb4af25eef556e886ef1ec4a3f2
jaynit1106 <jaynitpatel11062001@gmail.com>
2021-04-06 18:09:15 +0530

    comleted responsive

--- JS/index.js
+++ JS/index.js
@@ -4,6 +4,22 @@ let drawer=document.querySelector('.drawer');
 let times=document.querySelector('.times');
 let mobileNav=document.querySelector('.mobile-nav');
 
+// var x=window.matchMedia("(max-width: 480px)");
+// function mobile(x){
+// 	if(x.matches){
+// 		let right=document.querySelector('.right');
+// 		console.log(right.classList);
+// 		right.classList.remove('right');
+// 		console.log(right.classList);
+// 	}
+// }
+// mobile(x);
+// x.addListener(mobile);
+
+
+
+
+
 
 drawer.addEventListener('click',function(){
 	mobileNav.classList.add('open');
